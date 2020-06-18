3e22c247eb48ac1624de4ea9e9e300dc16867624
vijay0405 <uvijaysimha@gmail.com>
2020-06-18 23:22:47 +0530

    added js-search in backend to filter results

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const data = require('./data').userList;
+const JsSearch = require('js-search');
 
 const app = express();
+
+var search = new JsSearch.Search('id');
+search.addIndex('userId');
+search.addIndex(['title']);
+search.addIndex('body')
+
+search.addDocuments(data);
+
 
 
 app.use(bodyParser.json());
@@ -25,8 +34,8 @@ app.use((req, res, next) => {
 
 
 app.get("/posts", (req, res)=>{
-  console.log(req.query);
-  res.send(data);
+  console.log(req.query.userId);
+  res.send(search.search(req.query.userId));
 })
 
 module.exports = app;
